3f1c2dbf3f85102e8220179b19617c17cd70ceb0
KevinJordil <kevin.jordil@outlook.com>
2023-06-07 16:06:04 +0200

    install script*

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Get halide release
+wget https://github.com/halide/Halide/releases/download/v15.0.1/Halide-15.0.1-x86-64-linux-4c63f1befa1063184c5982b11b6a2cc17d4e5815.tar.gz
+
+# Unpack to ./halide
+mkdir halide
+tar -xzf Halide-15.0.1-x86-64-linux-4c63f1befa1063184c5982b11b6a2cc17d4e5815.tar.gz -C halide --strip-components=1

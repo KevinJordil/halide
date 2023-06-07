70061ded137b884ffc5b0c3622a161ec55fff2d2
KevinJordil <kevin.jordil@outlook.com>
2023-06-07 16:13:13 +0200

    Improve

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ wget https://github.com/halide/Halide/releases/download/v15.0.1/Halide-15.0.1-x8
 # Unpack to ./halide
 mkdir halide
 tar -xzf Halide-15.0.1-x86-64-linux-4c63f1befa1063184c5982b11b6a2cc17d4e5815.tar.gz -C halide --strip-components=1
+
+# Remove tar.gz file
+rm Halide-15.0.1-x86-64-linux-4c63f1befa1063184c5982b11b6a2cc17d4e5815.tar.gz
